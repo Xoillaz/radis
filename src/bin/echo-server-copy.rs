b362499965c6ef7ea54031fc27e6d6c925270eec
Xoillaz <xorillaz.persona@gmail.com>
2024-05-07 21:05:51 +0800

    feat: Implement echo server using asynchronous I/O

---
+++ src/bin/echo-server-copy.rs
@@ -0,0 +1,21 @@
+use tokio::io;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            // TcpStream::split split the stream into reader and writer handles,
+            // two of which should stay in the same scope with split.
+            let (mut rd, mut wr) = socket.split();
+
+            if io::copy(&mut rd, &mut wr).await.is_err() {
+                eprintln!("failed to copy");
+            }
+        });
+    }
+}
